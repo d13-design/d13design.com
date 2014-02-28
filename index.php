f3e4449900e97f3a99ce9efee58b40af4194f293
d13 <kdaulton@d13design.com>
2014-02-28 09:54:44 -0500

    Grid and content updates
    Added an even smaller grid size than xs
    Added gallery section

--- index.php
+++ index.php
@@ -64,7 +64,35 @@
         <div class="container">
             <h2>We dabble in all things fueled by creative thinking, whether it be visual or technical. We have delivered, top-notch solutions for projects ranging from social web applications to product design concepts. </h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio augue, congue pulvinar rutrum sit amet, sagittis ac enim. Vivamus vitae consectetur magna. Sed ornare turpis eu ante aliquam gravida. Maecenas vestibulum, erat eleifend blandit pharetra, neque ligula placerat erat, a rhoncus sem lectus in turpis. Ut posuere suscipit consequat. In hac habitasse platea dictumst. Donec ligula lorem, scelerisque a tincidunt in, bibendum at sem. Curabitur tincidunt, justo id sodales adipiscing, tellus magna pretium odio, at elementum est orci et est. Fusce purus tortor, feugiat mattis malesuada ut, lobortis vel dui. Nunc ornare tortor vitae lectus lobortis vulputate in eget dui.</p>
-            <p>Sed nisl sapien, venenatis non ante a, auctor auctor orci. Aliquam accumsan semper arcu, in luctus tellus luctus consectetur. Nulla pharetra felis sed odio dictum consectetur. Sed semper elementum eros, in accumsan est varius nec. Sed nec elit id sapien placerat suscipit a sit amet diam. Maecenas feugiat quam id urna porta lacinia. Nullam scelerisque ullamcorper sollicitudin. Cras commodo porttitor tincidunt. Duis tempor est quam, ut tincidunt diam posuere et. Donec viverra neque ac cursus tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+        <div class="gallery">
+            <div class="container-base">
+                <div class="row">
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="Sparx Landing"><img class="fluid" src="./assets/images/port/port_sparx_prev.png" alt="Sparx Landing"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="DPI Website"><img class="fluid" src="./assets/images/port/port_dpi_prev.png" alt="DPI Website"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="BHP Website"><img class="fluid" src="./assets/images/port/port_bhpm_prev.png" alt="BHP Website"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="ROJO Concept"><img class="fluid" src="./assets/images/port/port_rojo-v2_prev.png" alt="ROJO Concept"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="C&amp;R Prototype"><img class="fluid" src="./assets/images/port/port_candr_prev.png" alt="C&amp;R Prototype"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="RESTEE v1 App"><img class="fluid" src="./assets/images/port/port_restee-v1-app_prev.png" alt="RESTEE v1 App"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="Colors Logo"><img class="fluid" src="./assets/images/port/port_colors-logo_prev.png" alt="Colors Logo"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="Betterfly Homepage"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_betterfly_prev.png" alt="Betterfly Homepage"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="IES Prototype"><img class="fluid" src="./assets/images/port/port_ies_prev.png" alt="IES Prototype"/></div>
+
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="ROJO Website"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_rojosolutions_prev.png" alt="ROJO Website"/></div>
+                    <!-- <div class="col-6 col-xs-4 col-md-3 gallery-item"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_d13design_prev.png" alt=""/></div> -->
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="Dr. Ghersi Website"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_drghersi_prev.png" alt="Dr. Ghersi Website"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="CyberTek Website"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_cybertek_prev.png" alt="CyberTek Website"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="Bahamas Microsite"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_bahamas_prev.png" alt="Bahamas Microsite"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="DreamMaker Website"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_dreammaker_prev.png" alt="DreamMaker Website"/></div>
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="DScripted Website"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_dscripted_prev.png" alt="DScripted Website"/></div>
+                    <!-- <div class="col-6 col-xs-4 col-md-3 gallery-item"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_efrai_prev.png" alt=""/></div> -->
+                    <div class="col-6 col-xs-4 col-md-3 gallery-item" title="Rosenbaum Website"><img class="fluid" src="http://keithdaulton.com/assets/images/port/port_rosenbaumcontemporary_prev.png" alt="Rosenbaum Website"/></div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <h3>Sed nisl sapien, venenatis non ante a, auctor auctor orci. Aliquam accumsan semper arcu, in luctus tellus luctus consectetur. Nulla pharetra felis sed odio dictum consectetur.</h3>
+            <p> Sed semper elementum eros, in accumsan est varius nec. Sed nec elit id sapien placerat suscipit a sit amet diam. Maecenas feugiat quam id urna porta lacinia. Nullam scelerisque ullamcorper sollicitudin. Cras commodo porttitor tincidunt. Duis tempor est quam, ut tincidunt diam posuere et. Donec viverra neque ac cursus tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
             <p>Proin condimentum neque nec tempus fringilla. Morbi id lobortis lorem. Vivamus nisi dui, suscipit at dignissim sed, viverra eu mi. Nunc dapibus, lorem in aliquam rutrum, quam urna imperdiet tellus, a gravida nibh dolor nec mi. Proin accumsan ut diam et sollicitudin. Nunc consequat mauris sit amet arcu rutrum, ac faucibus massa cursus. Aliquam lectus augue, pretium eu adipiscing sed, sagittis quis velit. Proin eros ligula, ultricies id diam porta, tincidunt ullamcorper nisi. Vivamus aliquam mauris non eleifend porta.</p>
         </div>
     </div>
